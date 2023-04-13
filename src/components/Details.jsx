dff807f1e39b8950ccde29ce98b356df5dfae27e
ian <ianreeves19981@gmail.com>
2023-04-14 00:31:31 +0100

    test commit changed text in details.jsx

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Details() {
   const fakeArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
+  //test
   return (
     <div className="comments-container">
       <h2>
