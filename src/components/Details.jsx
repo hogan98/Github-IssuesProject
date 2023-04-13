6cb2de7cd5ac46b2a3d4d4119f60540524606074
ian <ianreeves19981@gmail.com>
2023-04-14 00:36:30 +0100

    new testt

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 export default function Details() {
   const fakeArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  //
   return (
     <div className="comments-container">
       <h2>
